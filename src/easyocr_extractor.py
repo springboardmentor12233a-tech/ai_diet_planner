86128bc6ed5deb19e6a23944c68bfd63ded5b57f
Naveenkrishna27 <naveenkrishna2774@gmail.com>
2026-02-22 11:28:38 +0530

    Delete src directory

--- src/easyocr_extractor.py
+++
@@ -1,9 +0,0 @@
-import easyocr
-import cv2
-
-reader = easyocr.Reader(['en'], gpu=False)
-
-def extract_text_from_image(image_path):
-    image = cv2.imread(image_path)
-    result = reader.readtext(image, detail=0)
-    return " ".join(result)
